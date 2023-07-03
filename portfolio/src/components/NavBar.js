64342f5aa8a4f82dac4e37e1ec509714d75b17a1
Mads Dyrved Møller <96830987+Mads784g@users.noreply.github.com>
2023-07-03 14:48:07 +0200

    Trying to get index.js deployed

---
+++ portfolio/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import "../CSS/NavBar.css"
+function NavBar() {
+    return(
+        <div>
+          <ul>
+            <li><a>Home</a></li>
+            <li><a>CV</a></li>
+            <li><a>Projects</a></li>
+          </ul>  
+        </div>
+    );
+}
+export default NavBar;
